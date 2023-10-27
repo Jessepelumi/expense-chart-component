bf79365204b7e5cfb6881afad0e8c438665890b5
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-10-27 14:43:32 +0100

    html strucutre done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          "soft-red": "hsl(10, 79%, 65%)",
+          "cyan": "hsl(186, 34%, 60%)"
+        },
+        neutral: {
+          "dark-brown": "hsl(25, 47%, 15%)",
+          "medium-brown": "hsl(28, 10%, 53%)",
+          "cream": "hsl(27, 66%, 92%)",
+          "pale-orange": "hsl(33, 100%, 98%)"
+        }
+      },
+      fontSize: {
+        "body-copy": "18px"
+      },
+      fontFamily: {
+         dmsans: ["DM Sans"]
+      }
+    },
   },
   plugins: [],
 }
